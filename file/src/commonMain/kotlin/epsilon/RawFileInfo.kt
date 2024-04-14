f32df3ca55474026f7c069821de9a52dd410b0b9
andylamax <andylamax@programmer.net>
2024-04-14 18:09:29 +0300

    Fixed the FileOutput things

--- file/src/commonMain/kotlin/epsilon/RawFileInfo.kt
+++ file/src/commonMain/kotlin/epsilon/RawFileInfo.kt
@@ -1,11 +1,7 @@
 package epsilon
 
 import koncurrent.Later
-import koncurrent.later.then
-import koncurrent.later.andThen
-import koncurrent.later.andZip
-import koncurrent.later.zip
-import koncurrent.later.catch
+import epsilon.RawFile
 
 expect class RawFileInfo(file: RawFile) {
     val file: RawFile
--- file/src/jsMain/kotlin/epsilon/RawFileInfo.kt
+++ file/src/jsMain/kotlin/epsilon/RawFileInfo.kt
@@ -4,7 +4,9 @@
 package epsilon
 
 import epsilon.internal.BrowserFileReader
+import epsilon.RawFile
 import koncurrent.Executors
+import koncurrent.Later
 
 actual class RawFileInfo actual constructor(actual val file: RawFile) {
     @Deprecated("in favour of nameWithExtension or nameWithoutExtension")
@@ -16,11 +18,17 @@ actual class RawFileInfo actual constructor(actual val file: RawFile) {
 
     actual val extension by lazy { file.name.substringAfterLast(".") }
 
-    actual fun path() = BrowserFileReader.native.readBase64Url(
-        blob = file,
-        executor = Executors.default(),
-        actionName = "constructing url for ${file.name}",
-        onAbortMessage = "File reading of ${file.name} has been aborted",
-        onErrorMessage = "Failed to read file: ${file.name}"
-    )
+    actual fun path() = paths.getOrPut(file) {
+        BrowserFileReader.native.readBase64Url(
+            blob = file,
+            executor = Executors.default(),
+            actionName = "constructing url for ${file.name}",
+            onAbortMessage = "File reading of ${file.name} has been aborted",
+            onErrorMessage = "Failed to read file: ${file.name}"
+        )
+    }
+
+    private companion object {
+        val paths = mutableMapOf<RawFile, Later<String>>()
+    }
 }
