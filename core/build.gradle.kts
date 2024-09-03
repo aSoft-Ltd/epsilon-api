e5b21c4a3ae39dd6a1483911a13ef986ccd3b9a1
andylamax <andylamax@programmer.net>
2024-09-03 04:30:41 +0300

    Fixed failint epsilon client build

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -9,9 +9,9 @@ description = "A kotlin multiplatform abstraction for reading files as blobs"
 kotlin {
     if (Targeting.JVM) jvm { library() }
     if (Targeting.JS) js(IR) { library() }
-//    if (Targeting.WASM) wasmJs { library() }
+    if (Targeting.WASM) wasmJs { library() }
 //    if (Targeting.WASM) wasmWasi { library() }
-    val osxTargets = if (Targeting.OSX) osxTargets() else listOf()
+    val osxTargets = if (Targeting.OSX) (iosTargets() + macOsTargets()) else listOf()
 //    val ndkTargets = if (Targeting.NDK) ndkTargets() else listOf()
     val linuxTargets = if (Targeting.LINUX) linuxTargets() else listOf()
 //    val mingwTargets = if (Targeting.MINGW) mingwTargets() else listOf()
@@ -40,11 +40,11 @@ kotlin {
             dependsOn(commonMain)
         }
 
-//        if(Targeting.WASM) {
-//            val wasmJsMain by getting {
-//                dependsOn(wasmMain)
-//            }
-//        }
+        if (Targeting.WASM) {
+            val wasmJsMain by getting {
+                dependsOn(wasmMain)
+            }
+        }
 
         val nativeMain by creating {
             dependsOn(commonMain)
